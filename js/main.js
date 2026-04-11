6285a31f5768f6d775265499cd9e0b38896d1a48
idenilson <idenilsonpenhasilva@gmail.com>
2026-04-11 18:34:33 -0300

    feat: show YouTube thumbnails with play button, lazy load iframe on click

--- js/main.js
+++ js/main.js
@@ -246,23 +246,20 @@ async function loadVideos() {
 
     emptyState.classList.add('hidden');
 
-    grid.innerHTML = data.videos.map(video => `
+    grid.innerHTML = data.videos.map(video => {
+        var vid = encodeURIComponent(video.youtube_id);
+        return `
         <div class="video-card">
-            <div class="video-embed">
-                <iframe
-                    src="https://www.youtube.com/embed/${encodeURIComponent(video.youtube_id)}"
-                    title="${escapeHTML(video.title)}"
-                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                    allowfullscreen
-                    loading="lazy"
-                ></iframe>
+            <div class="video-thumb" onclick="this.innerHTML='<iframe src=\\'https://www.youtube.com/embed/${vid}?autoplay=1\\' title=\\'${escapeHTML(video.title)}\\' allow=\\'accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture\\' allowfullscreen></iframe>'">
+                <img src="https://img.youtube.com/vi/${vid}/hqdefault.jpg" alt="${escapeHTML(video.title)}" loading="lazy">
+                <div class="play-btn"><svg viewBox="0 0 68 48" width="68" height="48"><path d="M66.52 7.74c-.78-2.93-2.49-5.41-5.42-6.19C55.79.13 34 0 34 0S12.21.13 6.9 1.55c-2.93.78-4.64 3.26-5.42 6.19C.06 13.05 0 24 0 24s.06 10.95 1.48 16.26c.78 2.93 2.49 5.41 5.42 6.19C12.21 47.87 34 48 34 48s21.79-.13 27.1-1.55c2.93-.78 4.64-3.26 5.42-6.19C67.94 34.95 68 24 68 24s-.06-10.95-1.48-16.26z" fill="red"/><path d="M45 24 27 14v20" fill="white"/></svg></div>
             </div>
             <div class="video-info">
                 <h4>${escapeHTML(video.title)}</h4>
                 ${video.description ? `<p>${escapeHTML(video.description)}</p>` : ''}
             </div>
         </div>
-    `).join('');
+    `}).join('');
 }
 
 /* ===== NAVIGATION ===== */
